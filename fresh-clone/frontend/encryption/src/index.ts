b79ac98007b880dbbdf45f08e4d7f7cde0209129
R34ddit <m.hans93@gmail.com>
2024-12-24 02:33:22 -0500

    clone cleanup

--- fresh-clone/frontend/encryption/src/index.ts
+++
@@ -1,5 +0,0 @@
-// index.ts
-// Entry point for the encryption module
-
-export { encryptData, decryptData, generateKey, } from './encryption';
-export { generateKeyPair, storeKey } from './keyManagement';
